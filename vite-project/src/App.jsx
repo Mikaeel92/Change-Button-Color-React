eccf4185b61fef8bd6232d3898dbf0e2bff6bcc7
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2024-12-28 14:35:22 +0330

    final step

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [bgColor, setBgcolor] = useState('#686868')
+
+  const handleClick = () => {
+    const random = '#' + Math.random().toString(16).slice(2,8)
+    setBgcolor(random)
+  }
+
   return (
-    <div className='bg-gray-300 w-screen h-screen'>App</div>
+    <div className='bg-gray-100 w-screen h-screen flex flex-col items-center justify-center'>
+      <button 
+      onClick={handleClick}
+      style={{background: `${bgColor}`}} 
+      className='p-4 text-white text-3xl rounded-md shadow-md'
+      >Change Color</button>
+    </div>
   )
 }
 
